0d6d3dd0cdcb8878fd34effdee93c43b0810f6a3
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-05 03:40:47 -0300

    secrets manager

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ terraform {
   }
 }
 
+#Ecr
 resource "aws_vpc_endpoint" "vpc_ecr_dkr" {
   service_name        = "com.amazonaws.us-east-1.ecr.dkr"
   vpc_endpoint_type   = "Interface"
@@ -33,6 +34,7 @@ resource "aws_vpc_endpoint" "vpc_ecr_dkr" {
   }
 }
 
+#Ecr
 resource "aws_vpc_endpoint" "vpc_ecr_api" {
   service_name      = "com.amazonaws.us-east-1.ecr.api"
   vpc_endpoint_type = "Interface"
@@ -52,6 +54,7 @@ resource "aws_vpc_endpoint" "vpc_ecr_api" {
   }
 }
 
+#S3
 resource "aws_vpc_endpoint" "vpc_s3_gtw" {
   service_name      = "com.amazonaws.us-east-1.s3"
   vpc_endpoint_type = "Gateway"
@@ -64,4 +67,64 @@ resource "aws_vpc_endpoint" "vpc_s3_gtw" {
     Name  = "endpoint-s3"
     infra = "vpc-endpoint-s3"
   }
+}
+
+#CloudWatch/Logs
+resource "aws_vpc_endpoint" "vpc_log" {
+  service_name = "com.amazonaws.us-east-1.logs"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  ip_address_type = "ipv4"
+
+  vpc_id = var.vpc_id
+  subnet_ids = [
+    var.subnet_a,
+    var.subnet_b
+  ]
+  security_group_ids = [var.sg_cluster_ecs]
+
+  tags = {
+    Name  = "endpoint-logs"
+    infra = "vpc-endpoint-logs"
+  }  
+}
+
+#RDS
+resource "aws_vpc_endpoint" "vpc_rds" {
+  service_name = "com.amazonaws.us-east-1.rds"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  ip_address_type = "ipv4"
+
+  vpc_id = var.vpc_id
+  subnet_ids = [
+    var.subnet_a,
+    var.subnet_b
+  ]
+  security_group_ids = [var.sg_cluster_ecs]
+
+  tags = {
+    Name  = "endpoint-rds"
+    infra = "vpc-endpoint-rds"
+  }  
+}
+
+#Secrets Manager
+resource "aws_vpc_endpoint" "vpc_sm" {
+  service_name = "com.amazonaws.us-east-1.secretsmanager"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  ip_address_type = "ipv4"
+
+  vpc_id = var.vpc_id
+  subnet_ids = [
+    var.subnet_a,
+    var.subnet_b
+  ]
+  security_group_ids = [var.sg_cluster_ecs]
+
+  tags = {
+    Name  = "endpoint-secrets-manager"
+    infra = "vpc-endpoint-sm"
+  }  
 }
